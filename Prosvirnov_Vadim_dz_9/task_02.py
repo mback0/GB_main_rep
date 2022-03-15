6baeac23f1b83bc4ca6b3f0614e3376b926715b1
mback <mback.edu@gmail.com>
2022-03-15 10:09:21 +0300

    complete task_2

---
+++ Prosvirnov_Vadim_dz_9/task_02.py
@@ -0,0 +1,23 @@
+class Road:
+    def __init__(self, length: int, width: int):
+        """конструктор класса
+        :param length: длинна в метрах
+        :param width: ширина в метрах
+        """
+        self._length = length
+        self._width = width
+
+    def calculate(self, height: int = 5, mass_m_2: int = 25) -> int:
+        """
+        считает масу массу асфальта, необходимого для покрытия всей дороги в тоннах
+        :param hight: высота дорожного полотна в сантиметрах
+        :param mass_m_2: масса в кг квадратного метра дороги высотой 1 см
+        :return: int значение тонн, дробная часть если есть НЕ учитывается
+        длина * ширина * масса асфальта для покрытия одного кв. метра дороги асфальтом, толщиной в 1 см * число см толщины полотна
+        """
+        res = int((self._width * self._length * mass_m_2 * height)/1000)
+        return res
+
+if __name__ == '__main__':
+    road = Road(5000, 20)
+    print(f'Для изготовления покрытия дороги нужно {road.calculate()} тонн.')
